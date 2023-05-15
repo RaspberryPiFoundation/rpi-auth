a7c1ae13943dede0232e4366626bbf21c0cadbad
Patrick Cherry <patch0@users.noreply.github.com>
2023-05-15 20:15:00 +0100

    Update config/routes.rb
    Co-authored-by: Greg Annandale <greg@raspberrypi.org>

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Dummy routes. These routes are never reached in the app, as Omniauth
   # intercepts it via Rack middleware before it reaches Rails, however adding
   # them allows us to use rpi_auth_login_path helpers etc.
-  post '/auth/rpi', as: :rpi_auth_login
+  post '/auth/rpi', as: :rpi_auth_login, params: { login_options: 'v1_signup' }
   post '/auth/rpi', as: :rpi_auth_signup, params: { login_options: 'force_signup,v1_signup' }
 
   namespace 'rpi_auth' do
