76ee6a4da7c26b7f2a13d95fb69648ae98929479
Patrick Cherry <patch0@users.noreply.github.com>
2023-05-22 19:48:52 +0100

    Refactor to set paths as constants, so they're consistent

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,9 @@ Rails.application.routes.draw do
   # Dummy routes. These routes are never reached in the app, as Omniauth
   # intercepts it via Rack middleware before it reaches Rails, however adding
   # them allows us to use rpi_auth_login_path helpers etc.
-  post '/auth/rpi', as: :rpi_auth_login, params: { login_options: 'v1_signup' }
-  post '/auth/rpi', as: :rpi_auth_signup, params: { login_options: 'force_signup,v1_signup' }
+  post RpiAuth::Engine::LOGIN_PATH, as: :rpi_auth_login, params: { login_options: 'v1_signup' }
+  post RpiAuth::Engine::LOGIN_PATH, as: :rpi_auth_signup, params: { login_options: 'force_signup,v1_signup' }
 
-  namespace 'rpi_auth' do
-    get '/auth/callback', to: 'auth#callback', as: 'callback'
-    get '/logout', to: 'auth#destroy', as: 'logout'
-  end
+  get RpiAuth::Engine::CALLBACK_PATH, to: 'rpi_auth/auth#callback', as: 'rpi_auth_callback'
+  get RpiAuth::Engine::LOGOUT_PATH, to: 'rpi_auth/auth#destroy', as: 'rpi_auth_logout'
 end
