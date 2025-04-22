42248336d571d85403479dc73e38b8ea6d834050
James Mead <james.mead+rpf@gofreerange.com>
2025-04-22 17:32:39 +0100

    Only use helper_method in CurrentUser if available
    In `code-club-frontend`, `RpiAuth::Controllers::CurrentUser` is being
    included into `AuthenticationConcern` which is in turn used in
    `ApplicationComponent` which is *not* a Rails controller and so doesn't
    support `AbstractController::Helpers::ClassMethods#helper_method`.

--- lib/rpi_auth/controllers/current_user.rb
+++ lib/rpi_auth/controllers/current_user.rb
@@ -6,7 +6,7 @@ module RpiAuth
       extend ActiveSupport::Concern
 
       included do
-        helper_method :current_user
+        helper_method :current_user if respond_to?(:helper_method)
       end
 
       def current_user
